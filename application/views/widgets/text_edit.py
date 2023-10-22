ae92267e5753a4ba56c0ba9548320d803aab747c
Lenu-com <oretyan@icloud.com>
2023-10-23 01:27:47 +0900

    [Add] text_edit.py

--- application/views/widgets/text_edit.py
+++ application/views/widgets/text_edit.py
@@ -1,10 +1,30 @@
 from PyQt6.QtWidgets import QTextEdit
 from PyQt6.QtCore import QSize
+from PyQt6.QtGui import QTextCursor
+from domain.text_edit.text_formatter import TextFormatter
 from typing import Final
 
 class TextEdit(QTextEdit):
-    SIZE: Final[QSize] = QSize(400, 600)
+    __SIZE: Final[QSize] = QSize(400, 600)
     
     def __init__(self):
         super().__init__()
-        self.resize(self.SIZE)
+        self.resize(self.__SIZE)
+        self.textChanged.connect(self.__apply_formatting)
+        
+    def __apply_formatting(self):
+        original_text = self.toPlainText()
+        formatted_text = TextFormatter().format_text(original_text)
+        
+        if formatted_text != original_text:
+            # textChanged シグナルの一時的な切断
+            self.textChanged.disconnect(self.__apply_formatting)
+            
+            # テキストの更新
+            self.setPlainText(formatted_text)
+            
+            # カーソルの位置を末尾に移動（これを行わないと、テキスト変更後にカーソルが先頭に移動してしまいます）
+            self.moveCursor(QTextCursor.MoveOperation.End)
+            
+            # textChanged シグナルの再接続
+            self.textChanged.connect(self.__apply_formatting)
