b32efff3f7ffbe4f2b4763ec519bf2d7a311d188
Lenu-com <oretyan@icloud.com>
2023-10-23 07:54:19 +0900

    [Refactor] Split functions into smaller, more focused units for better readability and maintainability.

--- application/domain/text_edit/text_formatter.py
+++ application/domain/text_edit/text_formatter.py
@@ -6,22 +6,33 @@ class TextFormatter:
     __CHAR_SIZE = {'full-width': 1.0, 'half-width': 0.5}
     __FULL_WIDTH_HEX = 0x3000
     
+    def __init__(self):
+        self.char_count: float = 0.0
+    
     def __is_full_width(self, char: str) -> bool:
         if ord(char) <= self.__FULL_WIDTH_HEX:
             return False
         return True
     
+    def __add_character_size(self, char: str) -> None:
+        if self.__is_full_width(char):
+            self.char_count += self.__CHAR_SIZE.get('full-width')
+        else:
+            self.char_count += self.__CHAR_SIZE.get('half-width')
+    
+    def __insert_newline(self,formatted_text: str) -> str:
+        if self.char_count >= self.__CHAR_LIMIT:
+            formatted_text += '\n'
+            self.char_count = 0
+        return formatted_text
+    
     def format_text(self, text: str) -> str:
         formatted_text = ''
-        char_count = 0
         
         for char in text:
             formatted_text += char
-            if self.__is_full_width(char):
-                char_count += self.__CHAR_SIZE.get('full-width')
-            else:
-                char_count += self.__CHAR_SIZE.get('half-width')
-            if char_count >= self.__CHAR_LIMIT:
-                formatted_text += '\n'
-                char_count = 0
+            self.__add_character_size(char)
+            # TODO Consider making formatted_text a member variable
+            formatted_text = self.__insert_newline(formatted_text)
+            
         return formatted_text
